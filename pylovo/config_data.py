3388a922f67494a272852884c98d633367a8a003
Matyas Hoffer-Toth <e12122086@student.tuwien.ac.at>
2025-03-13 13:20:48 +0100

    change type of occupants to integer in res

--- pylovo/config_data.py
+++ pylovo/config_data.py
@@ -65,7 +65,7 @@ CREATE_QUERIES = {
     comment varchar(80),
     free_walls integer,
     building_t varchar(80),
-    occupants numeric(23, 15),
+    occupants integer,
     floors integer,
     constructi varchar(80),
     refurb_wal numeric(23, 15),
