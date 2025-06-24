0b4b7fa134be8e2b9359f39423d77e25b4d9bad1
kadirrkalkann <kadir.kalkan@tum.de>
2025-06-24 16:59:33 +0200

    refactor ParameterCalculator class

--- src/classification/parameter_calculation/parameter_calculator.py
+++ src/classification/parameter_calculation/parameter_calculator.py
@@ -6,26 +6,24 @@ from src.config_loader import VERSION_ID
 
 
 class ParameterCalculator:
-    def __init__(self, plz):
-        self.plz = plz
-        # connect to database
+    def __init__(self):
         self.dbc = dbc.DatabaseClient()
-        self.pc = ParameterCalculator(str(self.plz))
 
-    def calc_parameters_for_grids(self) -> None:
+    def calc_parameters_for_grids(self, plz) -> None:
         """
         this function extracts and calculates parameters for each net in a plz
         """
         # check if for this version ID and PLZ the parameters have already been calculated
-        parameter_count = self.dbc.count_clustering_parameters(plz=self.plz)
+        plz = str(plz)
+        parameter_count = self.dbc.count_clustering_parameters(plz=plz)
         if parameter_count > 0:
             print(f"The parameters for the grids of postcode area {self.plz} and version {VERSION_ID} "
                   f"have already been calculated.")
             return
         # all nets within plz
-        cluster_list = self.dbc.get_list_from_plz(self.plz)
+        cluster_list = self.dbc.get_list_from_plz(plz)
         # loop over all networks
         for kcid, bcid in cluster_list:
-            gp = GridParameters(self.plz, bcid, kcid, self.dbc)
+            gp = GridParameters(plz, bcid, kcid, self.dbc)
             print(bcid, kcid)
             gp.calc_plz_parameters()
