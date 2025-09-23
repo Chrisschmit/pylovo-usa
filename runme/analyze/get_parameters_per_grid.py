95a94addec0951aeb236737f965614c611f52001
Christophe Schmit <ge69zat@mytum.de>
2025-09-23 19:19:09 +0200

    removed grid_per_parameters

--- runme/analyze/get_parameters_per_grid.py
+++
@@ -1,6 +0,0 @@
-# Must be run after get_parameters_per_regional_identifier.py
-from src.parameter_calculator import ParameterCalculator
-
-regional_identifier = 1
-pc = ParameterCalculator()
-pc.calc_parameters_per_grid(regional_identifier)
